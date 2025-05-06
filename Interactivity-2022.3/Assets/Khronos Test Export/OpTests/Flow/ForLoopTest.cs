157a118689df3d87a836a2c18cbeb322e64ff4ba
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-06 12:00:38 +0200

    added for spec tests to flow/for

--- Interactivity-2022.3/Assets/Khronos Test Export/OpTests/Flow/ForLoopTest.cs
+++ Interactivity-2022.3/Assets/Khronos Test Export/OpTests/Flow/ForLoopTest.cs
@@ -5,9 +5,11 @@ namespace Khronos_Test_Export
 {
     public class ForLoopTest : ITestCase
     {
-        private CheckBox bodyCheck;
-        private CheckBox loopRangeCheck;
-        private CheckBox completeCheck;
+        private CheckBox _bodyCheck;
+        private CheckBox _loopRangeCheck;
+        private CheckBox _completeCheck;
+        private CheckBox _initialIndexCheck;
+        private CheckBox _completedIndexCheck;
 
         public string GetTestName()
         {
@@ -21,9 +23,11 @@ namespace Khronos_Test_Export
         
         public void PrepareObjects(TestContext context)
         {
-            bodyCheck = context.AddCheckBox("Body flow");
-            loopRangeCheck = context.AddCheckBox("Loop range (0..10)");
-            completeCheck = context.AddCheckBox("Completed flow");
+            _bodyCheck = context.AddCheckBox("Body flow");
+            _loopRangeCheck = context.AddCheckBox("Loop range (0..10)");
+            _completeCheck = context.AddCheckBox("Completed flow");
+            _initialIndexCheck = context.AddCheckBox("Initial index");
+            _completedIndexCheck = context.AddCheckBox("Index when completed");
         }
 
         public void CreateNodes(TestContext context)
@@ -31,21 +35,33 @@ namespace Khronos_Test_Export
             var nodeCreator = context.interactivityExportContext;
             
             var forLoop = nodeCreator.CreateNode(new Flow_ForLoopNode());
-            context.SetEntryPoint(forLoop.FlowIn(Flow_ForLoopNode.IdFlowIn), "Loop Entry");
+            context.SetEntryPoint(out var entryFlow, "Loop Entry");
 
-            forLoop.Configuration[Flow_ForLoopNode.IdConfigInitialIndex].Value = 0;
+            forLoop.Configuration[Flow_ForLoopNode.IdConfigInitialIndex].Value = 1;
             forLoop.ValueIn(Flow_ForLoopNode.IdStartIndex).SetValue(0);
             forLoop.ValueIn(Flow_ForLoopNode.IdEndIndex).SetValue(10);
             
-            bodyCheck.SetupCheck(context, out var bodyCheckFlowIn);
+            _initialIndexCheck.SetupCheck(context, forLoop.ValueOut(Flow_ForLoopNode.IdIndex), out var initialIndexCheckFlow, 1);
+            context.AddSequence(entryFlow,
+                new FlowInRef[]
+                {
+                    initialIndexCheckFlow,
+                    forLoop.FlowIn(Flow_ForLoopNode.IdFlowIn),
+                });
+            
+            _bodyCheck.SetupCheck(context, out var bodyCheckFlowIn);
             
             context.AddPlusOneCounter(out var loopRangeCounter, out var flowInToIncrease);
-            loopRangeCheck.SetupCheck(context, loopRangeCounter, out var loopRangeCheckFlowIn, 10);
+            _loopRangeCheck.SetupCheck(context, loopRangeCounter, out var loopRangeCheckFlowIn, 10);
 
-            completeCheck.SetupCheck(context, out var completeCheckFlowIn);
+            _completeCheck.SetupCheck(context, out var completeCheckFlowIn);
+            
+            _completedIndexCheck.SetupCheck(context, forLoop.ValueOut(Flow_ForLoopNode.IdIndex), out var completedIndexCheckFlowIn, 10);
+            
             context.AddSequence( forLoop.FlowOut(Flow_ForLoopNode.IdCompleted),
                 new FlowInRef[]
                 {
+                    completedIndexCheckFlowIn,
                     loopRangeCheckFlowIn,
                     completeCheckFlowIn
                 });
