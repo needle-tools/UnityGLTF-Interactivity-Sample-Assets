2e98762da14d03005bb71faa6d6ce73a7349b719
Robert Dorn <dorn@prefrontalcortex.de>
2025-05-07 11:44:48 +0200

    added flow/multiGate test

---
+++ Interactivity-2022.3/Assets/Khronos Test Export/OpTests/Flow/MultiGateTest.cs
@@ -0,0 +1,151 @@
+using UnityGLTF.Interactivity.Export;
+using UnityGLTF.Interactivity.Schema;
+
+namespace Khronos_Test_Export
+{
+    public class MultiGateTest : ITestCase
+    {
+        private CheckBox _loopCheckBox;
+        private CheckBox _randomCheckBox;
+        private CheckBox _orderCheckBox;
+        private CheckBox _resetCheckBox;
+        
+        public string GetTestName()
+        {
+            return "flow/multiGate";
+        }
+
+        public string GetTestDescription()
+        {
+            return "";
+        }
+
+        public void PrepareObjects(TestContext context)
+        {
+            _loopCheckBox = context.AddCheckBox("Loop");
+            _randomCheckBox = context.AddCheckBox("Random (Check if all out flows are triggered once)");
+            _orderCheckBox = context.AddCheckBox("Order (008, 004, 001) > (001, 004, 008)");
+            _resetCheckBox = context.AddCheckBox("Reset Loop");
+        }
+
+        public void CreateNodes(TestContext context)
+        {
+            var nodeCreator = context.interactivityExportContext;
+            
+            // Order Tests
+            context.NewEntryPoint(out var entryOrder, _orderCheckBox.GetText());
+            var multiGateNode = nodeCreator.CreateNode(new Flow_MultiGateNode());
+            multiGateNode.FlowOut("008");
+            multiGateNode.FlowOut("004");
+            multiGateNode.FlowOut("001");
+            _orderCheckBox.SetupOrderFlowCheck(context, new FlowOutRef[]
+            {
+                multiGateNode.FlowOut("001"),
+                multiGateNode.FlowOut("004"),
+                multiGateNode.FlowOut("008"),
+            });
+            
+            context.AddSequence(entryOrder,
+                new FlowInRef[]
+                {
+                    multiGateNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                });
+            
+            
+            // Random Tests
+            var multiGateRandomNode = nodeCreator.CreateNode(new Flow_MultiGateNode());
+            context.NewEntryPoint(out var entryRandom, _randomCheckBox.GetText());
+            context.AddSequence(entryRandom,
+                new FlowInRef[]
+                {
+                    multiGateRandomNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateRandomNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateRandomNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateRandomNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                });
+            
+            multiGateRandomNode.Configuration[Flow_MultiGateNode.IdConfigIsRandom].Value = true;
+            _randomCheckBox.SetupMultiFlowCheck(context, 4, out var randomCheckFlowIn);
+            multiGateRandomNode.FlowOut("001").ConnectToFlowDestination(randomCheckFlowIn[0]);
+            multiGateRandomNode.FlowOut("002").ConnectToFlowDestination(randomCheckFlowIn[1]);
+            multiGateRandomNode.FlowOut("003").ConnectToFlowDestination(randomCheckFlowIn[2]);
+            multiGateRandomNode.FlowOut("004").ConnectToFlowDestination(randomCheckFlowIn[3]);
+            
+            
+            // Loop Tests
+            var multiGateLoopNode = nodeCreator.CreateNode(new Flow_MultiGateNode());
+            context.NewEntryPoint(out var entryLoop, _loopCheckBox.GetText());
+            context.AddSequence(entryLoop,
+                new FlowInRef[]
+                {
+                    multiGateLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                });
+            
+            multiGateLoopNode.Configuration[Flow_MultiGateNode.IdConfigIsLoop].Value = true;
+            _loopCheckBox.SetupMultiFlowCheck(context, 4, out var loopCheckFlowIn, new string[] {"Flow0", "Flow1", "Flow2", "Flow0 (2.)"});
+            
+            context.AddPlusOneCounter(out var loopCounter, out var flowInToIncrease);
+            var branchConditionSecondFlow = nodeCreator.CreateNode(new Math_EqNode());
+            branchConditionSecondFlow.ValueIn(Math_EqNode.IdValueA).SetValue(2);
+            branchConditionSecondFlow.ValueIn(Math_EqNode.IdValueB).ConnectToSource(loopCounter);
+            
+            var branchSecondFlow = nodeCreator.CreateNode(new Flow_BranchNode());
+            branchSecondFlow.ValueIn(Flow_BranchNode.IdCondition).ConnectToSource(branchConditionSecondFlow.FirstValueOut());
+            branchSecondFlow.FlowOut(Flow_BranchNode.IdFlowOutFalse).ConnectToFlowDestination(loopCheckFlowIn[0]);
+            branchSecondFlow.FlowOut(Flow_BranchNode.IdFlowOutTrue).ConnectToFlowDestination(loopCheckFlowIn[3]);
+            
+            context.AddSequence(multiGateLoopNode.FlowOut("001"),
+                new FlowInRef[]
+                {
+                    flowInToIncrease,
+                    branchSecondFlow.FlowIn(Flow_BranchNode.IdFlowIn)
+                });
+            
+            multiGateLoopNode.FlowOut("002").ConnectToFlowDestination(loopCheckFlowIn[1]);
+            multiGateLoopNode.FlowOut("003").ConnectToFlowDestination(loopCheckFlowIn[2]);
+            
+            // Reset Loop Tests
+            var multiGateResetLoopNode = nodeCreator.CreateNode(new Flow_MultiGateNode());
+            context.NewEntryPoint(out var entryLoopReset, _loopCheckBox.GetText());
+            context.AddSequence(entryLoopReset,
+                new FlowInRef[]
+                {
+                    multiGateResetLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateResetLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                    multiGateResetLoopNode.FlowIn(Flow_MultiGateNode.IdFlowInReset),
+                    multiGateResetLoopNode.FlowIn(Flow_MultiGateNode.IdFlowIn),
+                });
+
+            multiGateResetLoopNode.Configuration[Flow_MultiGateNode.IdConfigIsLoop].Value = true;
+            _resetCheckBox.SetupMultiFlowCheck(context, 3, out var loopResetCheckFlowIn);
+            
+            context.AddPlusOneCounter(out var loopCounterReset, out var flowInToIncreaseReset);
+            var branchConditionSecondFlowReset = nodeCreator.CreateNode(new Math_EqNode());
+            branchConditionSecondFlowReset.ValueIn(Math_EqNode.IdValueA).SetValue(2);
+            branchConditionSecondFlowReset.ValueIn(Math_EqNode.IdValueB).ConnectToSource(loopCounterReset);
+            
+            var branchSecondFlowReset = nodeCreator.CreateNode(new Flow_BranchNode());
+            branchSecondFlowReset.ValueIn(Flow_BranchNode.IdCondition).ConnectToSource(branchConditionSecondFlowReset.FirstValueOut());
+            branchSecondFlowReset.FlowOut(Flow_BranchNode.IdFlowOutFalse).ConnectToFlowDestination(loopResetCheckFlowIn[0]);
+            branchSecondFlowReset.FlowOut(Flow_BranchNode.IdFlowOutTrue).ConnectToFlowDestination(loopResetCheckFlowIn[2]);
+            
+            context.AddSequence(multiGateResetLoopNode.FlowOut("001"),
+                new FlowInRef[]
+                {
+                    flowInToIncreaseReset,
+                    branchSecondFlowReset.FlowIn(Flow_BranchNode.IdFlowIn)
+                });
+            
+            multiGateResetLoopNode.FlowOut("002").ConnectToFlowDestination(loopResetCheckFlowIn[1]);
+            // Add dummy connection to 003
+            context.AddSequence(multiGateResetLoopNode.FlowOut("003"), new FlowInRef[]
+            {
+            });
+        }
+    }
+}
